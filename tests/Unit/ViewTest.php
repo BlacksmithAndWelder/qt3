4c8e356292d78ca9924fdb800a69336ed51e33df
blacksmith <blacksmithandwelder@gmail.com>
2023-11-24 20:48:46 -0300

    ffff

--- tests/Unit/ViewTest.php
+++ tests/Unit/ViewTest.php
@@ -17,24 +17,36 @@ class ViewConfigTest extends TestCase
         $envMock = $this->getMockBuilder('EnvMock')
             ->setMethods(['env'])
             ->getMock();
-        
+
+        // Mocking the storage_path() function
+        $storagePathMock = $this->getMockBuilder('StoragePathMock')
+            ->setMethods(['storage_path'])
+            ->getMock();
+
         // Set up expectations for env() function
         $envMock->expects($this->any())
             ->method('env')
             ->willReturnMap([
-                ['VIEW_COMPILED_PATH', null, realpath(storage_path('framework/views'))],
+                ['VIEW_COMPILED_PATH', null, $storagePathMock->storage_path('framework/views')],
             ]);
+
+        // Set up expectations for storage_path() function
+        $storagePathMock->expects($this->any())
+            ->method('storage_path')
+            ->willReturnCallback(function ($path) {
+                return realpath(__DIR__ . '/../path/to/your/storage/' . $path);
+            });
 
         // Set up the expected configuration array
         $expectedConfig = [
             'paths' => [
-                resource_path('views'),
+                __DIR__ . '/../path/to/your/views',  // Adjust the path accordingly
             ],
-            'compiled' => realpath(storage_path('framework/views')),
+            'compiled' => realpath(__DIR__ . '/../path/to/your/storage/framework/views'),
         ];
 
         // Include the file to be tested
-        $config = include 'qt3/config/views.php';
+        $config = include __DIR__ . '/../path/to/your/config/views.php';
 
         // Verifying the configuration
         $this->assertEquals($expectedConfig, $config);
