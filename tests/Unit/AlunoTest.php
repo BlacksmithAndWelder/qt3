b377b27ab46d125e9a39e0afbaabbd7818e3a1b8
blacksmith <blacksmithandwelder@gmail.com>
2023-11-25 01:53:04 -0300

    3

--- tests/Unit/AlunoTest.php
+++ tests/Unit/AlunoTest.php
@@ -6,20 +6,21 @@ use App\Models\Aluno;
 use App\Models\Turma;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\View\Factory as ViewFactory; // Importe a classe ViewFactory
 
 class AlunoTest extends TestCase
 {
     public function testTurmaMethodReturnsHasOneRelation()
     {
         // Use a Factory do Eloquent para criar instâncias sem interação com o banco de dados
-        $aluno = Factory::factoryForModel(Aluno::class)->make([
+        $aluno = Factory::make(Aluno::class, [
             'nome' => 'João',
             'sobrenome' => 'Silva',
             'idade' => 20,
             'bolsa_estudos' => true,
             'turma_id' => 1,
         ]);
+
+        // Restante do teste permanece inalterado
         $result = $aluno->turma();
         $this->assertInstanceOf(HasOne::class, $result);
         $this->assertInstanceOf(Turma::class, $result->getRelated());
