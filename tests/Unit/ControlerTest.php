0e3935903d114689026b191f059f6969d998b9f1
blacksmith <blacksmithandwelder@gmail.com>
2023-11-24 23:50:18 -0300

    w

--- tests/Unit/ControlerTest.php
+++ tests/Unit/ControlerTest.php
@@ -1,48 +1,36 @@
 <?php
-use PHPUnit\Framework\TestCase;
+use Illuminate\View\View;
 use Mockery;
+use PHPUnit\Framework\TestCase;
 
 class SuporteTarefaControllerTest extends TestCase
 {
-    protected $suporteTarefaMock;
-
-    public function setUp(): void
-    {
-        $this->suporteTarefaMock = Mockery::mock('overload:App\Models\SuporteTarefa');
-    }
-
     public function tearDown(): void
     {
         Mockery::close();
     }
 
-    public function setSuporteTarefa($suporteTarefaMock)
-    {
-        $this->suporteTarefaMock = $suporteTarefaMock;
-    }
-
     public function testListar()
     {
         // Criar um mock para a classe de view
-        $viewMock = Mockery::mock('Illuminate\View\View');
-        
+        $viewMock = Mockery::mock(View::class);
+
         // Configurar comportamentos esperados para o mock de SuporteTarefa
-        $this->suporteTarefaMock->shouldReceive('with')->with(['usuario','status'])->andReturnSelf();
-        $this->suporteTarefaMock->shouldReceive('get')->andReturn([]);
+        $suporteTarefaMock = Mockery::mock('overload:App\Models\SuporteTarefa');
+        $suporteTarefaMock->shouldReceive('with')->with(['usuario', 'status'])->andReturnSelf();
+        $suporteTarefaMock->shouldReceive('get')->andReturn([]);
 
         // Criar uma instância do controlador
         $controller = new App\Http\Controllers\Web\Suporte\SuporteTarefaController();
 
-        // Substituir a instância do modelo SuporteTarefa pela mock
-        $controller->setSuporteTarefa($this->suporteTarefaMock);
-
-        // Substituir a função de view por um mock
-        $controller->setView($viewMock);
+        // Substituir a função de criação de views por um mock
+        app()->instance(View::class, $viewMock);
 
         // Chamar a função a ser testada
         $result = $controller->listar();
 
         // Asserção para verificar se o resultado é uma instância de view
-        $this->assertInstanceOf(\Illuminate\View\View::class, $result);
+        $this->assertInstanceOf(View::class, $result);
     }
 }
+
