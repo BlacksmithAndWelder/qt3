40dec7c8d8cd57ffdc193647168be75a95a355d0
blacksmith <blacksmithandwelder@gmail.com>
2023-11-25 20:32:31 -0300

    8

--- tests/Unit/Models/UserTest.php
+++ tests/Unit/Models/UserTest.php
@@ -1,6 +1,8 @@
 <?php
 use PHPUnit\Framework\TestCase;
 use App\Models\User;
+use App\Models\SuporteTarefaStatus;
+use App\Models\SuporteTarefa;
 
 class UserTest extends TestCase
 {
@@ -40,4 +42,33 @@ class UserTest extends TestCase
         $castsCompared = array_diff_assoc($castsExpected, $user->getCasts());
         $this->assertCount(0, $castsCompared, 'Algumas colunas casts não correspondem às expectativas.');
     }
+
+    public function testUSuporteTarefasRelationship()
+    {
+        // Create a mock of the SuporteTarefa model
+        $suporteTarefaMock = $this->createMock(SuporteTarefa::class);
+
+        // Create a mock of the SuporteTarefaStatus model and set up the expectations for the hasMany relationship
+        $suporteTarefaStatusMock = $this->getMockBuilder(SuporteTarefaStatus::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['hasMany'])
+            ->getMock();
+
+        // Expect the hasMany method to be called with specific arguments and return the SuporteTarefa mock
+        $suporteTarefaStatusMock->expects($this->once())
+            ->method('hasMany')
+            ->with(
+                $this->equalTo(SuporteTarefa::class),
+                $this->equalTo('status_id'),
+                $this->equalTo('id')
+            )
+            ->willReturn($suporteTarefaMock);
+
+        // Call the suporteTarefas method on the SuporteTarefaStatus model
+        $result = $suporteTarefaStatusMock->suporteTarefas();
+
+        // Assert that the result is an instance of SuporteTarefa
+        $this->assertInstanceOf(SuporteTarefa::class, $result);
+    }
 }
+
