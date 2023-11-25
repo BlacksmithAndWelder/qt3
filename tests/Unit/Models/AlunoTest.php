c4ace3b9278d19e2afa16bb2e661e8a6ab6ba2da
blacksmith <blacksmithandwelder@gmail.com>
2023-11-25 14:40:24 -0300

    r

--- tests/Unit/Models/AlunoTest.php
+++ tests/Unit/Models/AlunoTest.php
@@ -5,24 +5,34 @@ use App\Models\Turma;
 
 class AlunoTest extends TestCase
 {
-    public function testTurmaMethodReturnsTurmaModel()
+    public function testTurmaMethodReturnsTurmaRelation()
     {
-        // Criar uma instância do Aluno usando um mock manual
-        $aluno = new Aluno([
+        // Dados específicos do Aluno
+        $alunoData = [
             'nome' => 'João',
             'sobrenome' => 'Silva',
             'idade' => 20,
             'bolsa_estudos' => true,
             'turma_id' => 1,
-        ]);
+        ];
+
+        // Criar uma instância do Aluno usando um mock manual
+        $aluno = $this->getMockBuilder(Aluno::class)
+            ->setConstructorArgs([$alunoData])
+            ->getMock();
+
+        // Dados específicos da Turma
+        $turmaData = [
+            'escola_id' => 1,
+            'ativo' => true,
+            'equipe' => 'A',
+            'sala' => '101',
+        ];
 
         // Criar um mock manual para a classe Turma
         $turmaMock = $this->getMockBuilder(Turma::class)
-            ->onlyMethods(['find']) // Apenas mockar o método find
+            ->setConstructorArgs([$turmaData])
             ->getMock();
-
-        // Configurar o retorno desejado do método find
-        $turmaMock->method('find')->willReturn($turmaMock);
 
         // Substituir a implementação do método turma() no Aluno pelo mock de Turma
         $aluno->method('turma')->willReturn($turmaMock);
@@ -30,8 +40,8 @@ class AlunoTest extends TestCase
         // Chamar o método turma()
         $result = $aluno->turma();
 
-        // Verificar se o resultado é uma instância da classe Turma
-        $this->assertInstanceOf(Turma::class, $result);
+        // Verificar se o resultado é uma instância da relação HasOne
+        $this->assertInstanceOf(\Illuminate\Database\Eloquent\Relations\HasOne::class, $result);
 
         // Adicione mais verificações conforme necessário, dependendo da lógica real da sua aplicação
     }
