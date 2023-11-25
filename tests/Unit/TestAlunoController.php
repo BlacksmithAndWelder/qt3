2e28c8f256c3276a9cacc80dfb64186ded3c176a
blacksmith <blacksmithandwelder@gmail.com>
2023-11-24 21:35:10 -0300

    e

--- tests/Unit/TestAlunoController.php
+++ tests/Unit/TestAlunoController.php
@@ -1,33 +1,36 @@
 <?php
-namespace Tests\Unit;
 
+
+namespace Tests\Unit\Web\Aluno;
+
+use Tests\TestCase;
 use App\Http\Controllers\Web\Aluno\AlunoController;
 use App\Http\Requests\Aluno\Request as AlunoRequest;
 use App\Models\Aluno;
 use App\Models\Turma;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
-use Mockery;
-use Tests\TestCase;
 
 class AlunoControllerTest extends TestCase
 {
-    public function tearDown(): void
-    {
-        Mockery::close();
-    }
-
     /**
      * @codeCoverageIgnore
      */
     public function testListar()
     {
-        $alunoController = new AlunoController();
+        // Criar um mock para a classe Aluno
+        $alunoMock = $this->createMock(Aluno::class);
 
-        $alunoMock = Mockery::mock(Aluno::class);
-        $alunoMock->shouldReceive('get')->andReturn(collect());
+        // Configurar o mock para retornar uma coleção de alunos
+        $alunoMock->method('get')
+            ->willReturn(collect());
 
-        $this->assertInstanceOf(View::class, $alunoController->listar());
+        // Criar uma instância do controlador AlunoController
+        $controller = new AlunoController($alunoMock);
+
+        // Chamar a função listar e verificar o retorno
+        $result = $controller->listar();
+        $this->assertInstanceOf(View::class, $result);
     }
 
     /**
@@ -35,15 +38,22 @@ class AlunoControllerTest extends TestCase
      */
     public function testCriar()
     {
-        $alunoController = new AlunoController();
+        // Criar mocks para as classes Aluno e Turma
+        $alunoMock = $this->createMock(Aluno::class);
+        $turmaMock = $this->createMock(Turma::class);
 
-        $alunoMock = Mockery::mock(Aluno::class);
-        $turmaMock = Mockery::mock(Turma::class);
+        // Configurar o mock de Turma para retornar uma coleção de turmas
+        $turmaMock->method('where')
+            ->willReturn($turmaMock);
+        $turmaMock->method('get')
+            ->willReturn(collect());
 
-        $turmaMock->shouldReceive('where')->with('ativo', true)->andReturn($turmaMock);
-        $turmaMock->shouldReceive('get')->andReturn(collect());
+        // Criar uma instância do controlador AlunoController
+        $controller = new AlunoController($alunoMock, $turmaMock);
 
-        $this->assertInstanceOf(View::class, $alunoController->criar());
+        // Chamar a função criar e verificar o retorno
+        $result = $controller->criar();
+        $this->assertInstanceOf(View::class, $result);
     }
 
     /**
@@ -51,26 +61,110 @@ class AlunoControllerTest extends TestCase
      */
     public function testSalvar()
     {
-        $alunoController = new AlunoController();
+        // Criar mocks para as classes Aluno, Turma e AlunoRequest
+        $alunoMock = $this->createMock(Aluno::class);
+        $turmaMock = $this->createMock(Turma::class);
+        $requestMock = $this->createMock(AlunoRequest::class);
 
-        $requestMock = Mockery::mock(AlunoRequest::class);
-        $turmaMock = Mockery::mock(Turma::class);
+        // Configurar o mock de AlunoRequest para retornar dados validados
+        $requestMock->method('validated')
+            ->willReturn([
+                'nome' => 'John Doe',
+                'sobrenome' => 'Doe',
+                'idade' => 25,
+                'bolsa_estudos' => true,
+                'turma_id' => 1,
+            ]);
 
-        $turmaMock->shouldReceive('find')->andReturn($turmaMock);
+        // Configurar mocks para Turma e Aluno
+        $turmaMock->method('find')
+            ->willReturn($turmaMock);
+        $alunoMock->method('create')
+            ->willReturn($alunoMock);
 
-        $requestMock->shouldReceive('validated')->andReturn([
-            'nome' => 'Nome Teste',
-            'sobrenome' => 'Sobrenome Teste',
-            'idade' => 20,
-            'bolsa_estudos' => true,
-            'turma_id' => 1,
-        ]);
+        // Criar uma instância do controlador AlunoController
+        $controller = new AlunoController($alunoMock, $turmaMock);
 
-        $alunoMock = Mockery::mock(Aluno::class);
-        $alunoMock->shouldReceive('create')->andReturn($alunoMock);
-
-        $this->assertInstanceOf(RedirectResponse::class, $alunoController->salvar($requestMock));
+        // Chamar a função salvar e verificar o redirecionamento
+        $result = $controller->salvar($requestMock);
+        $this->assertInstanceOf(RedirectResponse::class, $result);
     }
 
-    // Adicione mais testes para as outras funções do controlador conforme necessário
+    /**
+     * @codeCoverageIgnore
+     */
+    public function testEditar()
+    {
+        // Criar mocks para as classes Aluno e Turma
+        $alunoMock = $this->createMock(Aluno::class);
+        $turmaMock = $this->createMock(Turma::class);
+
+        // Configurar mocks para Aluno e Turma
+        $alunoMock->method('with')
+            ->willReturn($alunoMock);
+        $turmaMock->method('where')
+            ->willReturn($turmaMock);
+        $turmaMock->method('get')
+            ->willReturn(collect());
+
+        // Criar uma instância do controlador AlunoController
+        $controller = new AlunoController($alunoMock, $turmaMock);
+
+        // Chamar a função editar e verificar o retorno
+        $result = $controller->editar(1);
+        $this->assertInstanceOf(View::class, $result);
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function testAtualizar()
+    {
+        // Criar mocks para as classes Aluno, Turma e AlunoRequest
+        $alunoMock = $this->createMock(Aluno::class);
+        $turmaMock = $this->createMock(Turma::class);
+        $requestMock = $this->createMock(AlunoRequest::class);
+
+        // Configurar mocks para Aluno, Turma e AlunoRequest
+        $alunoMock->method('find')
+            ->willReturn($alunoMock);
+        $turmaMock->method('find')
+            ->willReturn($turmaMock);
+        $requestMock->method('validated')
+            ->willReturn([
+                'nome' => 'John Doe',
+                'sobrenome' => 'Doe',
+                'idade' => 25,
+                'bolsa_estudos' => true,
+                'turma_id' => 1,
+            ]);
+
+        // Criar uma instância do controlador AlunoController
+        $controller = new AlunoController($alunoMock, $turmaMock);
+
+        // Chamar a função atualizar e verificar o redirecionamento
+        $result = $controller->atualizar($requestMock, 1);
+        $this->assertInstanceOf(RedirectResponse::class, $result);
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function testExcluir()
+    {
+        // Criar mocks para as classes Aluno e Turma
+        $alunoMock = $this->createMock(Aluno::class);
+
+        // Configurar mock para Aluno
+        $alunoMock->method('find')
+            ->willReturn($alunoMock);
+        $alunoMock->method('delete');
+
+        // Criar uma instância do controlador AlunoController
+        $controller = new AlunoController($alunoMock);
+
+        // Chamar a função excluir e verificar o redirecionamento
+        $result = $controller->excluir(1);
+        $this->assertInstanceOf(RedirectResponse::class, $result);
+    }
 }
