daecbcdbcb0ff7dc129492f865524b5656d2b9cd
blacksmith <blacksmithandwelder@gmail.com>
2023-11-26 17:00:25 -0300

    h

--- tests/Unit/testedosTest.php
+++ tests/Unit/testedosTest.php
@@ -4,19 +4,26 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use Tests\TestCase;
+use Mockery\MockInterface;
 
 class ApiTest extends TestCase
 {
     use RefreshDatabase;
 
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Configuração e criação do mock para DB facade
+        $this->mockDatabase = $this->mock(DB::class, function (MockInterface $mock) {
+            $queryBuilderMock = $this->mock('alias:' . Illuminate\Database\Query\Builder::class);
+            $queryBuilderMock->shouldReceive('select')->andReturnSelf(); // Pode adicionar outras expectativas conforme necessário
+            $mock->shouldReceive('table')->andReturn($queryBuilderMock);
+        });
+    }
+
     public function testApiRoute()
     {
-        // Faz o mock do facade DB para simular o acesso ao banco de dados
-        DB::shouldReceive('table')
-            ->once()
-            ->with('users')
-            ->andReturn(['user' => 'mocked_user']);
-
         // Simula uma requisição GET para a rota /api/user
         $response = $this->get('/api/user');
 
