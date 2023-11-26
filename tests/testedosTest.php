b490d0e85a847b06e9891308e8e7e5848d4c4dcf
blacksmith <blacksmithandwelder@gmail.com>
2023-11-26 13:02:21 -0300

    r

--- tests/testedosTest.php
+++ tests/testedosTest.php
@@ -1,6 +1,8 @@
 <?php
 use App\Http\Controllers\Web\Suporte\SuporteTarefaController;
 use App\Models\SuporteTarefa;
+use App\Models\User;
+use App\Models\SuporteTarefaStatus;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\View\View;
 use Tests\TestCase;
@@ -9,33 +11,60 @@ class SuporteTarefaControllerTest extends TestCase
 {
     public function testListarFunction()
     {
-        // Criar alguns dados de teste usando a instância do Eloquent Collection
-        $mockedData = new Collection([
-            new SuporteTarefa(['assunto' => 'Assunto 1', 'descricao' => 'Descrição 1']),
-            new SuporteTarefa(['assunto' => 'Assunto 2', 'descricao' => 'Descrição 2']),
-            // Adicionar mais dados conforme necessário
-        ]);
-
         // Criar uma instância do controlador
         $controller = new SuporteTarefaController();
 
-        // Chamar a função 'listar' com os dados mockados
-        $response = $controller->listar($mockedData);
+        // Mock da classe SuporteTarefa para simular o comportamento do Eloquent
+        $suporteTarefaMock = $this->createMock(SuporteTarefa::class);
+
+        // Simular um resultado para a função with do Eloquent
+        $suporteTarefaMock->expects($this->once())
+            ->method('with')
+            ->willReturnSelf();
+
+        // Simular um resultado para a função get do Eloquent
+        $suporteTarefaMock->expects($this->once())
+            ->method('get')
+            ->willReturn(new Collection([])); // Pode adicionar dados mockados conforme necessário
+
+        // Substituir a instância real por nosso mock
+        $controller->setSuporteTarefa($suporteTarefaMock);
+
+        // Chamar a função 'listar'
+        $response = $controller->listar();
 
         // Verificar se a resposta é uma instância de View
         $this->assertInstanceOf(View::class, $response);
 
         // Verificar se a view correta está sendo retornada
         $this->assertEquals('suporte-tarefa.listar', $response->getName());
-
-        // Verificar se os dados mockados estão sendo passados para a view
-        $this->assertEquals($mockedData, $response->getData()['ListaSuporteTarefa']);
     }
 
-        public function testCriarFunction()
+    public function testCriarFunction()
     {
         // Criar uma instância do controlador
         $controller = new SuporteTarefaController();
+
+        // Mock das classes User e SuporteTarefaStatus para simular o comportamento do Eloquent
+        $userMock = $this->createMock(User::class);
+        $statusMock = $this->createMock(SuporteTarefaStatus::class);
+
+        // Mock da classe SuporteTarefa para simular a criação de uma nova tarefa
+        $suporteTarefaMock = $this->createMock(SuporteTarefa::class);
+
+        // Simular um usuário e status para o novo SuporteTarefa
+        $userMock->expects($this->once())
+            ->method('find')
+            ->willReturnSelf();
+
+        $statusMock->expects($this->once())
+            ->method('find')
+            ->willReturnSelf();
+
+        // Substituir as instâncias reais por nossos mocks
+        $controller->setUser($userMock);
+        $controller->setStatus($statusMock);
+        $controller->setSuporteTarefa($suporteTarefaMock);
 
         // Chamar a função 'criar'
         $response = $controller->criar();
@@ -46,6 +75,4 @@ class SuporteTarefaControllerTest extends TestCase
         // Verificar se a view correta está sendo retornada
         $this->assertEquals('suporte-tarefa.criar', $response->getName());
     }
-
-
 }
