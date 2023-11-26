20ee2462591b7969ebcfc9dd7e38645f3fb893ee
blacksmith <blacksmithandwelder@gmail.com>
2023-11-25 22:37:02 -0300

    w

--- tests/Unit/Middleware/Test.php
+++ tests/Unit/Middleware/Test.php
@@ -6,25 +6,34 @@ use App\Models\SuporteTarefaStatus;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\View;
-use Database\Factories\UserFactory;
 
 class SuporteTarefaControllerTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        // Load the model factories
-        $this->withFactories(__DIR__.'/database/factories');
-    }
-
     public function testListarFunction()
     {
-        // Use the UserFactory to create instances of User with predefined values
-        $user = UserFactory::new()->create();
+        // Create a user
+        $user = User::create([
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+            'password' => bcrypt('password'),
+        ]);
 
-        // Use Laravel's model factory to create instances with predefined values
-        $mockedResponse = factory(SuporteTarefa::class, 2)->create(['user_id' => $user->id]);
+        // Create SuporteTarefa instances directly
+        $suporteTarefa1 = SuporteTarefa::create([
+            'status_id' => 1,
+            'user_id' => $user->id,
+            'assunto' => 'Support Task 1',
+            'descricao' => 'Description for Support Task 1',
+            'urgente' => false,
+        ]);
+
+        $suporteTarefa2 = SuporteTarefa::create([
+            'status_id' => 2,
+            'user_id' => $user->id,
+            'assunto' => 'Support Task 2',
+            'descricao' => 'Description for Support Task 2',
+            'urgente' => true,
+        ]);
 
         // Mock the SuporteTarefa model using Laravel's model mocking
         SuporteTarefa::shouldReceive('with')
@@ -34,7 +43,8 @@ class SuporteTarefaControllerTest extends TestCase
         // Use Laravel's testing helpers to assert the view response
         $response = app(SuporteTarefaController::class)->listar();
 
+        // Assert that the view is correct and contains the expected data
         $response->assertViewIs('suporte-tarefa.listar');
-        $response->assertViewHas('ListaSuporteTarefa', $mockedResponse->toArray());
+        $response->assertViewHas('ListaSuporteTarefa', [$suporteTarefa1->toArray(), $suporteTarefa2->toArray()]);
     }
 }
