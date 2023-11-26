8f61653723a67d161e8d45bc42f12ca6e44c6224
blacksmith <blacksmithandwelder@gmail.com>
2023-11-26 14:52:55 -0300

    s

--- tests/Unit/testedosTest.php
+++ tests/Unit/testedosTest.php
@@ -1,29 +1,17 @@
 <?php
-use App\Http\Controllers\Web\Suporte\SuporteTarefaController;
 use App\Models\SuporteTarefa;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\View;
-use Mockery\MockInterface;
+use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 
 class STCTest extends TestCase
 {
+    use RefreshDatabase; // Usar o trait RefreshDatabase para resetar o banco de dados a cada teste
+
     public function testListarFunction()
     {
-        // Criar uma coleção de tarefas mockadas
-        $mockedTarefas = [
-            new SuporteTarefa(['assunto' => 'Assunto 1', 'descricao' => 'Descrição 1']),
-            new SuporteTarefa(['assunto' => 'Assunto 2', 'descricao' => 'Descrição 2']),
-        ];
-
-        // Criar um mock específico para a classe SuporteTarefa
-        $suporteTarefaMock = \Mockery::mock(SuporteTarefa::class, function (MockInterface $mock) use ($mockedTarefas) {
-            $mock->shouldReceive('with')->andReturnSelf();
-            $mock->shouldReceive('get')->andReturn(collect($mockedTarefas));
-        });
-
-        // Substituir a implementação real pelo mock
-        $this->app->instance(SuporteTarefa::class, $suporteTarefaMock);
+        // Criar algumas tarefas no banco de dados de teste
+        SuporteTarefa::factory()->count(2)->create();
 
         // Chamar a rota que corresponde à função 'listar'
         $response = $this->get(route('suporte-tarefa.listar'));
@@ -34,13 +22,7 @@ class STCTest extends TestCase
         // Verificar se a view 'suporte-tarefa.listar' está sendo retornada
         $response->assertViewIs('suporte-tarefa.listar');
 
-        // Verificar se os dados das tarefas mockadas estão presentes na view
-        $response->assertViewHas('ListaSuporteTarefa', $mockedTarefas);
-    }
-
-    // Liberar os recursos do Mockery após o teste
-    protected function tearDown(): void
-    {
-        \Mockery::close();
+        // Verificar se a view tem a variável 'ListaSuporteTarefa' que contém as tarefas do banco de dados
+        $response->assertViewHas('ListaSuporteTarefa');
     }
 }
