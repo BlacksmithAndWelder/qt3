6f258cbf728eafadcc6f3755e9d3a116ab373663
blacksmith <blacksmithandwelder@gmail.com>
2023-11-25 22:15:34 -0300

    w

--- tests/Unit/Middleware/Test.php
+++ tests/Unit/Middleware/Test.php
@@ -6,59 +6,39 @@ use App\Models\SuporteTarefaStatus;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\View\View;
+use Illuminate\Support\Facades\View as ViewFacade;
 
 class SuporteTarefaControllerTest extends TestCase
 {
     public function testListarFunction()
     {
-        // Mock the SuporteTarefa model to simulate the with method behavior
-        $suporteTarefaModelMock = $this->getMockBuilder(SuporteTarefa::class)
-            ->onlyMethods(['with', 'get'])
-            ->getMock();
-
-        // Expect a call to with method with an array of relationships
-        $suporteTarefaModelMock->expects($this->once())
-            ->method('with')
-            ->with($this->equalTo(['usuario', 'status']))
-            ->willReturnSelf();
-
-        // Simulate a response for the get method
+        // Simulate a response with hardcoded values
         $mockedResponse = new Collection([
             new SuporteTarefa(['assunto' => 'Assunto 1', 'descricao' => 'Descrição 1']),
             new SuporteTarefa(['assunto' => 'Assunto 2', 'descricao' => 'Descrição 2']),
             // Add more mock data as needed
         ]);
 
-        // Set the results directly on the model mock
-        $suporteTarefaModelMock->expects($this->once())
-            ->method('get')
-            ->willReturn($mockedResponse);
-
         // Mock the view instance
         $viewMock = $this->getMockBuilder(View::class)->getMock();
 
-        // Mock the SuporteTarefaController to override the behavior of the SuporteTarefa model
-        $controllerMock = $this->getMockBuilder(SuporteTarefaController::class)
-            ->onlyMethods(['listar', 'view'])
-            ->getMock();
+        // Mock the View facade to override the behavior of the view method
+        ViewFacade::shouldReceive('make')
+            ->with('suporte-tarefa.listar', ['ListaSuporteTarefa' => $mockedResponse])
+            ->andReturn($viewMock);
 
-        // Set up the expectation for the view method to return the mocked view instance
-        $controllerMock->expects($this->once())
-            ->method('view')
-            ->willReturn($viewMock);
+        // Create an instance of the SuporteTarefaController
+        $controller = new SuporteTarefaController();
 
-        // Set up the expectation for the listar method to return the mocked model instance
-        $controllerMock->expects($this->once())
-            ->method('listar')
-            ->willReturn($suporteTarefaModelMock);
-
-        // Call the mocked listar method
-        $response = $controllerMock->listar();
+        // Call the listar method
+        $response = $controller->listar();
 
         // Assert that the response is the mocked SuporteTarefa model instance
-        $this->assertSame($suporteTarefaModelMock, $response);
+        $this->assertSame($mockedResponse, $response);
 
-        // Assert that the view method was called with the correct parameters
-        $controllerMock->view('suporte-tarefa.listar', ['ListaSuporteTarefa' => $mockedResponse]);
+        // Assert that the View facade was called with the correct parameters
+        ViewFacade::shouldHaveReceived('make')
+            ->with('suporte-tarefa.listar', ['ListaSuporteTarefa' => $mockedResponse])
+            ->once();
     }
 }
