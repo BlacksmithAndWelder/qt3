f1cfc9b965e0a98aa83d795cf6a2df8dff9597a7
blacksmith <blacksmithandwelder@gmail.com>
2023-11-26 12:53:30 -0300

    t

--- tests/testedosTest.php
+++ tests/testedosTest.php
@@ -31,4 +31,21 @@ class SuporteTarefaControllerTest extends TestCase
         // Verificar se os dados mockados estão sendo passados para a view
         $this->assertEquals($mockedData, $response->getData()['ListaSuporteTarefa']);
     }
+
+        public function testCriarFunction()
+    {
+        // Criar uma instância do controlador
+        $controller = new SuporteTarefaController();
+
+        // Chamar a função 'criar'
+        $response = $controller->criar();
+
+        // Verificar se a resposta é uma instância de View
+        $this->assertInstanceOf(View::class, $response);
+
+        // Verificar se a view correta está sendo retornada
+        $this->assertEquals('suporte-tarefa.criar', $response->getName());
+    }
+
+
 }
