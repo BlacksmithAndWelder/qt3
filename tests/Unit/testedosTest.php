57ea5ffbda7d692f05e653c58f9647daef1825e6
blacksmith <blacksmithandwelder@gmail.com>
2023-11-28 08:43:17 -0300

    e

--- tests/Unit/testedosTest.php
+++ tests/Unit/testedosTest.php
@@ -1,4 +1,5 @@
 <?php
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Mockery as m;
 use Tests\TestCase;
 use App\Http\Controllers\Web\Turma\TurmaController;
@@ -8,25 +9,48 @@ use App\Models\Turma;
 
 class TurmaControllerTest extends TestCase
 {
+    use RefreshDatabase; // Este trait recria o banco de dados antes de cada teste
+
     public function tearDown(): void
     {
         m::close();
     }
 
+    public function testListar()
+    {
+        // Cria um mock para a classe Turma
+        $mockTurma = m::mock(Turma::class);
+        $mockTurma->shouldReceive('with->get')->andReturn(['item1', 'item2', 'item3']);
+
+        // Cria uma instância do controlador passando o mockTurma
+        $controller = new TurmaController($mockTurma);
+
+        // Chama o método a ser testado
+        $view = $controller->listar();
+
+        // Faz as asserções dos resultados
+        $this->assertEquals('turma.listar', $view->name());
+        $this->assertEquals(['item1', 'item2', 'item3'], $view->getData()['ListaTurma']);
+    }
+
     public function testCriar()
     {
+        // Cria mocks para Turma e Escola
         $mockTurma = m::mock(Turma::class);
         $mockEscola = m::mock(Escola::class);
         $mockEscola->shouldReceive('get')->andReturn(['escola1', 'escola2']);
 
+        // Cria uma instância do controlador passando os mocks
         $controller = new TurmaController($mockTurma, $mockEscola);
 
+        // Chama o método a ser testado
         $view = $controller->criar();
 
+        // Faz as asserções dos resultados
         $this->assertEquals('turma.criar', $view->name());
         $this->assertInstanceOf(Turma::class, $view->getData()['Turma']);
         $this->assertEquals(['escola1', 'escola2'], $view->getData()['ListaEscolas']);
     }
 
-    // Adicione outros testes para os métodos 'listar', 'salvar', 'editar', 'atualizar', 'excluir' conforme necessário
+    // Adicione outros testes para os métodos 'salvar', 'editar', 'atualizar', 'excluir' conforme necessário
 }
