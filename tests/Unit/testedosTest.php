7efa830706bfb121f0fed4883ac375f21adf3d0f
blacksmith <blacksmithandwelder@gmail.com>
2023-11-28 09:26:58 -0300

    e

--- tests/Unit/testedosTest.php
+++ tests/Unit/testedosTest.php
@@ -2,19 +2,23 @@
 use Tests\TestCase;
 #use PHPUnit\Framework\TestCase;
 use App\Models\Turma;
+
 class TurmaControllerTest extends TestCase
 {
     public function testListar()
     {
-
         // Criar um mock manual para a classe Turma
-        $mockTurma = $this->createMock(Turma::class);
+        $mockTurma = $this->getMockBuilder(Turma::class)
+            ->onlyMethods(['with', 'toArray']) // Adicione outros métodos que você espera chamar
+            ->getMock();
+        
         $mockTurma->expects($this->once())
             ->method('with')
             ->willReturnSelf();
+
         $mockTurma->expects($this->once())
-            ->method('get')
-            ->willReturn(['item1', 'item2', 'item3']);
+            ->method('toArray')
+            ->willReturn(['item1', 'item2', 'item3']); // Pode ajustar para refletir o formato real do retorno
 
         // Criar uma instância do controlador passando o mockTurma
         $controller = new TurmaController($mockTurma);
