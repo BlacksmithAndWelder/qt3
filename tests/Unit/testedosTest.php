9d660fd699bf48499939b709bb1580b109aeaafb
blacksmith <blacksmithandwelder@gmail.com>
2023-11-28 09:51:26 -0300

    r

--- tests/Unit/testedosTest.php
+++ tests/Unit/testedosTest.php
@@ -1,6 +1,6 @@
 <?php
-use Tests\TestCase;
-#  use PHPUnit\Framework\TestCase;
+#use Tests\TestCase;
+use PHPUnit\Framework\TestCase;
 use App\Http\Controllers\Web\Turma\TurmaController;
 
 class TurmaControllerTest extends TestCase
