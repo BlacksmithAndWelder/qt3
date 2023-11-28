b14932d8da98b08898e7e297342ded3306feaa31
blacksmith <blacksmithandwelder@gmail.com>
2023-11-28 20:55:09 -0300

    z

--- tests/Unit/testedosTest.php
+++ tests/Unit/testedosTest.php
@@ -1,16 +1,25 @@
 <?php
 
-use App\Http\Controllers\AlunoController;
+use App\Http\Controllers\Web\Aluno\AlunoController;
 use App\Http\Requests\Aluno\Request as AlunoRequest;
+use App\Models\Aluno;
+use App\Models\Turma;
 use Illuminate\Validation\ValidationException;
+use Mockery\MockInterface;
 use PHPUnit\Framework\TestCase;
 
 class AlunoControllerTest extends TestCase
 {
+    public function tearDown(): void
+    {
+        Mockery::close();
+        parent::tearDown();
+    }
+
     public function testCreateAlunoWithValidData()
     {
-        $request = $this->createMock(AlunoRequest::class);
-        $request->method('all')->willReturn([
+        $request = Mockery::mock(AlunoRequest::class);
+        $request->shouldReceive('validated')->andReturn([
             'nome' => 'João',
             'sobrenome' => 'Silva',
             'idade' => 25,
@@ -19,16 +28,16 @@ class AlunoControllerTest extends TestCase
         ]);
 
         $controller = new AlunoController();
-        $response = $controller->createAluno($request);
+        $response = $controller->salvar($request);
 
-        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(302, $response->getStatusCode());
         // Adicione mais asserções conforme necessário
     }
 
     public function testCreateAlunoWithInvalidData()
     {
-        $request = $this->createMock(AlunoRequest::class);
-        $request->method('all')->willReturn([
+        $request = Mockery::mock(AlunoRequest::class);
+        $request->shouldReceive('validated')->andReturn([
             // Dados inválidos, por exemplo, falta o nome
             'sobrenome' => 'Silva',
             'idade' => 25,
@@ -39,13 +48,13 @@ class AlunoControllerTest extends TestCase
         $this->expectException(ValidationException::class);
 
         $controller = new AlunoController();
-        $controller->createAluno($request);
+        $controller->salvar($request);
     }
 
     public function testCreateAlunoWithDatabaseError()
     {
-        $request = $this->createMock(AlunoRequest::class);
-        $request->method('all')->willReturn([
+        $request = Mockery::mock(AlunoRequest::class);
+        $request->shouldReceive('validated')->andReturn([
             'nome' => 'João',
             'sobrenome' => 'Silva',
             'idade' => 25,
@@ -53,12 +62,21 @@ class AlunoControllerTest extends TestCase
             'turma_id' => 1,
         ]);
 
-        $mockedDatabase = $this->createMock(Database::class);
-        $mockedDatabase->method('insert')->willReturn(false);
+        $controller = new AlunoController();
 
-        $controller = new AlunoController($mockedDatabase);
-        
-        $this->expectException(DatabaseException::class);
-        $controller->createAluno($request);
+        // Mock do método create da classe Aluno
+        $alunoMock = Mockery::mock(Aluno::class);
+        $alunoMock->shouldReceive('create')->andReturnUsing(
+            function ($attributes) {
+                // Simulando um erro de banco de dados
+                return $attributes['nome'] === 'Erro' ? false : true;
+            }
+        );
+
+        // Substitui o método create do Aluno pelo mock criado acima
+        $this->app->instance(Aluno::class, $alunoMock);
+
+        $this->expectException(\Exception::class);
+        $controller->salvar($request);
     }
 }
