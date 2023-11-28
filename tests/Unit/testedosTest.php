dac0ef619e3a77e34f90afeb59d93b78b1d46b62
blacksmith <blacksmithandwelder@gmail.com>
2023-11-28 09:12:49 -0300

    s

--- tests/Unit/testedosTest.php
+++ tests/Unit/testedosTest.php
@@ -1,27 +1,24 @@
 <?php
-use Mockery as m;
 use PHPUnit\Framework\TestCase;
-use App\Http\Controllers\Web\Turma\TurmaController;
-use App\Models\Turma;
 
 class TurmaControllerTest extends TestCase
 {
-    public function tearDown(): void
-    {
-        m::close();
-    }
-
     public function testListar()
     {
-        // Cria um mock para a classe Turma
-        $mockTurma = m::mock(Turma::class);
-        $mockTurma->shouldReceive('with->get')->andReturn(['item1', 'item2', 'item3']);
+        // Criar um mock manual para a classe Turma
+        $mockTurma = $this->createMock(Turma::class);
+        $mockTurma->expects($this->once())
+            ->method('with')
+            ->willReturnSelf();
+        $mockTurma->expects($this->once())
+            ->method('get')
+            ->willReturn(['item1', 'item2', 'item3']);
 
-        // Cria uma instância do controlador passando o mockTurma
+        // Criar uma instância do controlador passando o mockTurma
         $controller = new TurmaController($mockTurma);
 
         // Chama diretamente o método a ser testado, evitando a resolução automática de dependências
-        $view = $controller->listar($mockTurma);
+        $view = $controller->listar();
 
         // Faz as asserções dos resultados
         $this->assertEquals('turma.listar', $view->name());
