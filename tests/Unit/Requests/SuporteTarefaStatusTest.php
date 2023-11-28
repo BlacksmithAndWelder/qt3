e7a0bf1c4a42c8af6032c56e530bc58aeb34967c
blacksmith <blacksmithandwelder@gmail.com>
2023-11-28 20:35:30 -0300

    q

--- tests/Unit/Requests/SuporteTarefaStatusTest.php
+++ tests/Unit/Requests/SuporteTarefaStatusTest.php
@@ -5,22 +5,21 @@ use PHPUnit\Framework\TestCase;
 
 class RequestTest extends TestCase
 {
-   // public function testRules()
-   //  {
-    //     // Criar uma instância da classe Request (a classe que estamos testando)
-   //      $request = $this->getMockBuilder(Request::class)
-    //         ->disableOriginalConstructor()
-  //           ->onlyMethods(['rules']) // Definir apenas os métodos que queremos substituir
-     //        ->getMock();
- //
-        // Substituir a implementação da função rules
-  //       $request->expects($this->any())
-    //         ->method('rules')
-    //        ->willReturn(['nome' => 'required|string|max:20|in:Aberto,Inconsistente,Solucionado,Recusado']);
- //
+ public function testRules()
+  {
+    // Criar uma instância da classe Request (a classe que estamos testando)
+      $request = $this->getMockBuilder(Request::class)
+         ->disableOriginalConstructor()
+         ->onlyMethods(['rules']) // Definir apenas os métodos que queremos substituir
+         ->getMock();
+         // Substituir a implementação da função rules
+      $request->expects($this->any())
+           ->method('rules')
+           ->willReturn(['nome' => 'required|string|max:20|in:Aberto,Inconsistente,Solucionado,Recusado']);
+ 
     //     // Chamar a função rules e verificar se o resultado é o esperado
-   //      $this->assertEquals(['nome' => 'required|string|max:20|in:Aberto,Inconsistente,Solucionado,Recusado'], $request->rules());
-    // }
+       $this->assertEquals(['nome' => 'required|string|max:20|in:Aberto,Inconsistente,Solucionado,Recusado'], $request->rules());
+   }
 
     public function testAuthorize()
     {
